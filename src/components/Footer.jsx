00055063bec32dd9439494a2daf9f28bf62e042a
jubayerahmmad <zubayersami510@gmail.com>
2024-12-04 12:52:26 +0600

    slider in home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,36 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
+import logo from "../assets/visapilot-logo.png";
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="footer footer-center bg-gray-50 p-10">
+      <aside>
+        <img className="w-48" src={logo} alt="" />
+
+        <p className="font-lato font-bold text-gray-500">
+          Unlock your visa journey with{" "}
+          <span className="font-extrabold text-gray-800">Visa Pilot</span>.
+          Explore visa options, apply with ease, and track your progress.
+        </p>
+      </aside>
+      <nav>
+        <div className="grid grid-flow-col gap-4 text-gray-600">
+          <a className="cursor-pointer hover:text-[#3B9DF8]">
+            <FaInstagram size={32}></FaInstagram>
+          </a>
+          <a className="cursor-pointer hover:text-[#3B9DF8]">
+            <FaFacebook size={32}></FaFacebook>
+          </a>
+          <a className="cursor-pointer hover:text-[#3B9DF8]">
+            <FaLinkedin size={32}></FaLinkedin>
+          </a>
+        </div>
+        <div className="divider"></div>
+        <p className="text-gray-600">
+          <span className="font-bold text-cyan-400">VisaPilot</span> ©{" "}
+          {new Date().getFullYear()} - All right reserved
+        </p>
+      </nav>
+    </footer>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => {
   const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between w-full relative  boxShadow rounded-full px-[10px] py-[8px] font-montserrat">
+    <nav className="flex items-center justify-between w-full relative  boxShadow px-[10px] py-[8px] font-montserrat bg-gray-50">
       <Link to="/">
         <img src={logo} alt="logo" className="w-32 object-cover" />
       </Link>
-      <ul className="items-center gap-[20px] text-2xl text-[#424242] lg:flex hidden font-semibold">
+      <ul className="items-center gap-[20px] text-2xl  lg:flex hidden font-semibold">
         <NavLink to="/">
           <li className="hover:text-[#3B9DF8]">Home</li>
         </NavLink>
@@ -31,13 +31,13 @@ const Header = () => {
 
       <div className="items-center gap-[10px] flex">
         <Link to="/register">
-          <button className="py-[7px] text-lg px-[16px] rounded-full capitalize bg-[#3B9DF8] text-white hover:bg-blue-400 transition-all duration-300 sm:flex hidden">
+          <button className="py-[8px] font-semibold text-lg px-[16px] rounded-full capitalize bg-[#3B9DF8] text-white hover:bg-blue-400 transition-all duration-300 sm:flex hidden">
             Register
           </button>
         </Link>
 
         <CiMenuFries
-          className="text-[1.8rem] mr-1 text-[#424242]c cursor-pointer lg:hidden flex"
+          className="text-[1.8rem] mr-1 cursor-pointer lg:hidden flex"
           onClick={() => setMobileSidebarOpen(!mobileSidebarOpen)}
         />
       </div>
@@ -50,27 +50,27 @@ const Header = () => {
         } lg:hidden bg-white boxShadow p-4 text-center absolute top-[65px] right-0 w-full rounded-md transition-all duration-300`}
       >
         <ul className="items-center gap-[20px] text-[1rem] text-gray-600 flex flex-col text-xl">
-          <NavLink>
+          <NavLink to="/">
             <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer">
               Home
             </li>
           </NavLink>
-          <NavLink>
+          <NavLink to="/allVisas">
             <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer">
               All Visas
             </li>
           </NavLink>
-          <NavLink>
+          <NavLink to="/addVisa">
             <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer">
               Add Visa
             </li>
           </NavLink>
-          <NavLink>
+          <NavLink to="/myAddedVisas">
             <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer">
               My Added Visas
             </li>
           </NavLink>
-          <NavLink>
+          <NavLink to="/myVisaApplicaton">
             <li className="hover:border-b-[#3B9DF8] border-b-[2px] border-transparent transition-all duration-500 cursor-pointer">
               My Visa Applications
             </li>
